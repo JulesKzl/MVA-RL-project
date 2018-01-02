dad7a7ffd506250d78b6dd864959d4ea5329ab62
Jules Kzl <jules.kozolinsky@ens-cachan.fr>
2018-01-02 14:48:57 +0100

    continue

--- code/experiment.py
+++ code/experiment.py
@@ -23,14 +23,15 @@ def run_experiment(agent, env, nb_simu, N_samples, seed=1):
     for k in tqdm(range(nb_simu), desc="Simulating {}".format(agent.name)):
         # Compute policy π ̃k:
         agent.update_policy()
-        print(agent.policy)
+        # print(agent.policy)
         # Execute policy π ̃k on environnement during a max of max_duration:
         agent.execute_policy(env, max_duration)
 
         # Computing regret at eatch step for plotting
         E_list = []
         for i in range(N_samples):
-            E = env.compute_LTAR(agent.policy, T_max/N_samples)
+            # E = env.compute_LTAR(agent.policy, T_max/N_samples)
+            E = env.compute_regret(agent.policy, T_max/N_samples)
             E_list.append(E)
         E_mean = np.array(E_list).mean()
         E_var = np.array(E_list).var()
@@ -39,6 +40,7 @@ def run_experiment(agent, env, nb_simu, N_samples, seed=1):
     x = np.arange(1, nb_simu+1)
     plt.plot(x, E_eps_mean, label = agent.name)
     plt.xlabel('Rounds')
-    plt.ylabel('Average Long-term reward')
+    # plt.ylabel('Average Long-term reward')
+    plt.ylabel('Regret')
     plt.legend()
     plt.show()
--- code/run_exp.py
+++ code/run_exp.py
@@ -4,8 +4,8 @@ import env_examples as env
 import algos as ag
 import experiment as exp
 
-# env_TP1 = env.make_MDP_TP1()
-env_TP1 = env.make_riverSwim()
+env_TP1 = env.make_MDP_TP1()
+# env_TP1 = env.make_riverSwim()
 max_reward = 5
 agent = ag.PSRL(env_TP1, max_reward)
 # agent = ag.UCRL2(env_TP1, max_reward)
